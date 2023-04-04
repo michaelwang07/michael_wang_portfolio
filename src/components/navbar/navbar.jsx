2d943c1db6538f650bce7de9dc3bdf02814ee48e
michaelwang07 <michaelwang07@yahoo.com>
2023-04-04 03:42:08 -0700

    navbar initial

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import './navbar.css'
+import {HiOutlineMenu} from 'react-icons/hi'
 
 const navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
-    <div>navbar</div>
+    <div className='app__navbar'>
+      <div className='app__navbar-mynmae'>
+        <a href="">
+          <h1 className='header-1'>Michael Wang</h1>
+        </a>
+      </div>
+      <ul className='app__navbar-links'>
+        <li>
+          <a href="#home" className='content-1'>Home</a>
+        </li>
+        <li>
+          <a href="#about" className='content-1'>About</a>
+        </li>
+        <li>
+          <a href="#resume" className='content-1'>Resume</a>
+        </li>
+        <li>
+          <a href="#contact" className='content-1'>Contact</a>
+        </li>
+      </ul>
+      {toggleMenu && (
+        <div className='app__navbar-smallscreen'>
+        <HiOutlineMenu fontSize={30} className='menu' onClick={() => setToggleMenu(true)} />
+        <ul className='app__navbar-smallscreen_links'>
+          <li>
+            <a href="#home" className='content-1'>Home</a>
+          </li>
+          <li>
+            <a href="#about" className='content-1'>About</a>
+          </li>
+          <li>
+            <a href="#resume" className='content-1'>Resume</a>
+          </li>
+          <li>
+            <a href="#contact" className='content-1'>Contact</a>
+          </li>
+        </ul>
+      </div>
+      )}
+    </div>
   )
 }
 
