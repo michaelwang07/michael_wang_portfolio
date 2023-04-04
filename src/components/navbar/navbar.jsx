5ba9c417c17e5ff4142d62c4ad9a665566e5daf8
michaelwang07 <michaelwang07@yahoo.com>
2023-04-04 05:05:26 -0700

    navbar fixes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css'
 import {HiOutlineMenu} from 'react-icons/hi'
 
 const navbar = () => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleMenu, setToggleMenu] = React.useState(true);
 
   return (
     <div className='app__navbar'>
@@ -14,37 +14,46 @@ const navbar = () => {
       </div>
       <ul className='app__navbar-links'>
         <li>
-          <a href="#home" className='content-1'>Home</a>
+          <a href="#home" className='content-2'>Home</a>
         </li>
         <li>
-          <a href="#about" className='content-1'>About</a>
+          <a href="#about" className='content-2'>About</a>
         </li>
         <li>
-          <a href="#resume" className='content-1'>Resume</a>
+          <a href="#resume" className='content-2'>Resume</a>
         </li>
         <li>
-          <a href="#contact" className='content-1'>Contact</a>
+          <a href="#contact" className='content-2'>Contact</a>
         </li>
       </ul>
-      {toggleMenu && (
-        <div className='app__navbar-smallscreen'>
-        <HiOutlineMenu fontSize={30} className='menu' onClick={() => setToggleMenu(true)} />
-        <ul className='app__navbar-smallscreen_links'>
+      <div className='app__navbar-smallscreen'>
+        <div className='app__navbar-smallscreen_menu'>
+          <HiOutlineMenu fontSize={30} className='menu' onClick={() => {
+            if(toggleMenu) {
+              setToggleMenu(false);
+            } else {
+              setToggleMenu(true);
+            }
+          }} />
+        </div>
+        {toggleMenu && (
+          <ul className='app__navbar-smallscreen_links'>
           <li>
-            <a href="#home" className='content-1'>Home</a>
+            <a href="#home" className='content-2'>Home</a>
           </li>
           <li>
-            <a href="#about" className='content-1'>About</a>
+            <a href="#about" className='content-2'>About</a>
           </li>
           <li>
-            <a href="#resume" className='content-1'>Resume</a>
+            <a href="#resume" className='content-2'>Resume</a>
           </li>
           <li>
-            <a href="#contact" className='content-1'>Contact</a>
+            <a href="#contact" className='content-2'>Contact</a>
           </li>
         </ul>
+        )}
       </div>
-      )}
+
     </div>
   )
 }
