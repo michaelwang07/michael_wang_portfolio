525aae58403ed20e6f2633887e5a4b4d8ca4dfe8
michaelwang07 <michaelwang07@yahoo.com>
2023-04-14 05:49:03 -0700

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/michael_wang_portfoilo/',
+  base: '/michael_wang_portfolio/',
   plugins: [react()],
 })
